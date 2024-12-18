b9cfc36aa05b90ff545703fd10590298e512fb49
sumonrst <eng.sumonrejata@gmail.com>
2024-12-18 12:04:28 +0600

    video section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+const text = document.querySelector(".text");
+text.innerHTML = text.innerText
+	.split("")
+	.map(
+		(char, i) => `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`
+	)
+	.join("");
+
+
 
 $(document).ready(function() {
     $('.services_area__services').slick({
