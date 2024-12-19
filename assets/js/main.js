4da3b07e2efaa066b6277e91dfdc164a4e49b680
sumonrst <eng.sumonrejata@gmail.com>
2024-12-19 12:48:00 +0600

    header sticky done

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,27 @@ $(document).ready(function() {
         slidesToShow: 2,
     });
 });
+
+/* Header Sticky Menu Code here */ 
+$(window).scroll(function() {
+    /* Header top social sticky start */
+    if($(window).scrollTop() > 130) {
+        $('.headertop').addClass('hide-header-top');
+    }else{
+        $('.headertop').removeClass('hide-header-top');
+    }
+
+    /* Header middle sticky start */
+    if($(window).scrollTop() > 130) {
+        $('.headermiddle').addClass('headermiddle-remove');
+    }else{
+        $('.headermiddle').removeClass('headermiddle-remove');
+    }
+
+    /* Header middle sticky start */
+    if($(window).scrollTop() > 130) {
+        $('.header__area').addClass('padding-remove');
+    }else{
+        $('.header__area').removeClass('padding-remove');
+    }
+});
